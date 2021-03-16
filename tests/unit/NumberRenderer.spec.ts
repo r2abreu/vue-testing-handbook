046b118ee27874a89cfc7505d7cac75565a8df93
r2abreu <r2abreu@pm.me>
2021-03-16 21:13:59 +0100

    Implemented computed property and refactor closes #7

--- tests/unit/NumberRenderer.spec.ts
+++ tests/unit/NumberRenderer.spec.ts
@@ -1,13 +1,19 @@
 import { mount } from '@vue/test-utils';
 import NumberRenderer from '@/components/NumberRenderer.vue';
 
+interface Props {
+  even: boolean;
+}
+
+const factory = (propsData: Props) => mount(NumberRenderer, { propsData: { ...propsData } });
+
 describe('NumberRenderer', () => {
   it('renders even numbers', () => {
-    const wrapper = mount(NumberRenderer, {
-      propsData: {
-        even: true,
-      },
-    });
+    const wrapper = factory({ even: true });
     expect(wrapper.text()).toBe('2, 4, 6, 8');
+  });
+  it('renders odd numbers', () => {
+    const wrapper = factory({ even: false });
+    expect(wrapper.text()).toBe('1, 3, 5, 7, 9');
   });
 });
