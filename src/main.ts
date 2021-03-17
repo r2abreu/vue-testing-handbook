238f11a17d130d379cdb07b0210fd2c893c481ae
r2abreu <r2abreu@pm.me>
2021-03-17 11:06:58 +0100

    Add async submit, mock not working pending refacto closes #11

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-Vue.config.productionTip = false;
-
 new Vue({
-  render: (h) => h(App),
+  // eslint-disable-next-line arrow-parens
+  render: h => h(App),
 }).$mount('#app');
--- tests/unit/FormSubmitter.spec.ts
+++ tests/unit/FormSubmitter.spec.ts
@@ -1,5 +1,19 @@
 import { mount } from '@vue/test-utils';
+import flushPromises from 'flush-promises';
 import FormSubmitter from '@/components/FormSubmitter.vue';
+
+let url = '';
+let data = '';
+
+const mockHttp = {
+  get: (_url: string, _data: string) =>
+    // eslint-disable-next-line arrow-parens
+    new Promise<void>(resolve => {
+      url = _url;
+      data = _data;
+      resolve();
+    }),
+};
 
 describe('FormSubmitter.vue', () => {
   it('doesnt render message without submitting', () => {
@@ -13,6 +27,25 @@ describe('FormSubmitter.vue', () => {
 
     await wrapper.find('[type="text"]').setValue('Arturo');
     await button.trigger('click');
+
+    expect(wrapper.find('.message').text()).toBe('Thank you for your submission, Arturo.');
+  });
+  it('reveals a notification when submitted', async () => {
+    const wrapper = mount(FormSubmitter, {
+      data() {
+        return {
+          asyncTest: true,
+        };
+      },
+      mocks: {
+        axios: mockHttp,
+      },
+    });
+
+    await wrapper.find('[type="text"]').setValue('Arturo');
+    await wrapper.find('form').trigger('submit');
+
+    await flushPromises();
 
     expect(wrapper.find('.message').text()).toBe('Thank you for your submission, Arturo.');
   });
